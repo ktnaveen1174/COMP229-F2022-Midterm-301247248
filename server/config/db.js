ec968e5e4872505f39ac3a862c65788c4341d367
ktnaveen1174 <nkanmani@my.centennialcollege.ca>
2022-10-31 19:16:13 -0400

    Second Commit

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,7 @@
 module.exports = 
 {
     //"URI": "mongodb://localhost/book_store"
-    "URI": "mongodb+srv://thomas:Hq3DKYN2NaAqGyyV@mongodbserver.k15hk.azure.mongodb.net/book_store?retryWrites=true&w=majority",
+    //"URI": "mongodb+srv://thomas:Hq3DKYN2NaAqGyyV@mongodbserver.k15hk.azure.mongodb.net/book_store?retryWrites=true&w=majority",
+    "URI": "mongodb+srv://ktnaveen:ktnaveen@cluster0.sidjmpo.mongodb.net/midterm_bookstore?retryWrites=true&w=majority",
     "Secret": 'SomeSecret'
 }
