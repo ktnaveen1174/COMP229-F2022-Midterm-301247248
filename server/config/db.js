87f43c258966c3ad8373c6492b0521578907068b
ktnaveen1174 <nkanmani@my.centennialcollege.ca>
2022-11-02 10:07:38 -0400

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,6 @@
 module.exports = 
 {
-    "URI":"mongodb+srv://aafaqmuzaffar:Nzsbgyfttgmx6aAH@booklist.f6kvdlh.mongodb.net/bookstore?retryWrites=true&w=majority"
-    
+    //"URI":"mongodb+srv://aafaqmuzaffar:Nzsbgyfttgmx6aAH@booklist.f6kvdlh.mongodb.net/bookstore?retryWrites=true&w=majority"
+   "URI": "mongodb+srv://ktnaveen:ktnaveen@cluster0.sidjmpo.mongodb.net/midterm?retryWrites=true&w=majority",
+   "Secret":'SomeSecret'
 }
